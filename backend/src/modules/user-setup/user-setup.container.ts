61740430962111a70c1ae31d3e0a61adad3b8d72
jadson-js <jadson20051965@gmail.com>
2026-03-05 22:10:07 -0300

    fix

--- backend/src/modules/user-setup/user-setup.container.ts
+++ backend/src/modules/user-setup/user-setup.container.ts
@@ -1,6 +1,12 @@
 import { container } from "tsyringe";
 import { UserSetupController } from "@/modules/user-setup/user-setup.controller";
+import { CreateUserSetupWithSetupStacksPrismaRepository } from "@/infra/database/prisma/user-setup/create-user-setup-with-setup-stacks.repository";
 import { CreateUserSetupUseCase } from "@/modules/user-setup/use-cases/create-user-setup";
+
+// Registra o repositório
+container.register("CreateUserSetupWithSetupStacksRepository", {
+  useClass: CreateUserSetupWithSetupStacksPrismaRepository,
+});
 
 // Registra os use cases
 container.register("CreateUserSetupUseCase", {
