9a4ce11eb3ab9915d252aaf5534992d17609e18d
jadson-js <jadson20051965@gmail.com>
2026-03-22 09:32:06 -0300

    chore: remove Swagger documentation setup and dependencies.

--- backend/src/infra/http/server.ts
+++ backend/src/infra/http/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import swaggerUi from "swagger-ui-express";
 import routes from "./routes";
 import { errorHandler } from "./middlewares/error.middleware";
 import { env } from "@/shared/env";
-import { swaggerSpec } from "./swagger";
 
 const app = express();
 
@@ -17,11 +15,6 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.get("/api/docs.json", (_, response) => {
-  response.json(swaggerSpec);
-});
 
 app.use(routes);
 
